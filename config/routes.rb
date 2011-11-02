d98601f258f4314841804ca9bd73b43ee8e2d4ec
demonslayer <margaret.k.mcnulty@gmail.com>
2011-11-02 15:32:33 -0600

    Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Dinnerwithfamily::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/contact"
+
+  get "pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
