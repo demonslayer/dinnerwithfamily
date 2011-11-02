806116965246434c771019a078df507d35e3837b
demonslayer <margaret.k.mcnulty@gmail.com>
2011-11-02 15:00:23 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinnerwithfamily::Application.config.secret_token = '5becd44e14955a89e7ccd1730ff6001603d77c6321278ae7f009f34a8d35582ea13e494bf13781811fb926753a4a8b21ec5d243567928065b857af40bc8142a7'
