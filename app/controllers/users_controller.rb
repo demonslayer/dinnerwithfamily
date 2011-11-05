d46bf160f7d8ac2a6cf01e31b7f4acf238799e65
demonslayer <margaret.k.mcnulty@gmail.com>
2011-11-05 16:02:09 -0600

    Users can now sign up and create robots

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      @user.level = 1;
+      @user.save
+      flash[:success] = "Welcome to Dinner with Family!"
+      redirect_to @user
+    else
+      @title = "Create User"
+      render 'new'
+    end
+  end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -34,5 +34,57 @@ describe UsersController do
     end
     
   end
+  
+  describe "POST 'create'" do
+    
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :name => "", :robot => "", :password => "", :password_confirmation => ""}
+      end
+      
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+      
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Create User")
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+      
+    end
+    
+    describe "success" do
+      
+      before(:each) do
+        @attr = { :name => "Kaiya" , :robot => "databot.png", :password => "foo", :password_confirmation => "foo" }
+      end
+      
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+      
+      it "should redirect to the show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+      
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to Dinner with Family/i
+      end
+      
+    end
+      
+  end
 
 end
