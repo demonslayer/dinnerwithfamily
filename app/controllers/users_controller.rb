cbe6e95a447cd158ee94b34adcb482a5255d39b9
demonslayer <margaret.k.mcnulty@gmail.com>
2011-11-10 18:35:35 -0700

    Revert "Can now edit user information"
    This reverts commit ed3977c0f97f8dd5a5708552e7333ceb1755edfc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :update]
-  before_filter :correct_user, :only => [:edit, :update]
-  
   def new
     @title = "Create User"
     @user = User.new
@@ -26,32 +23,6 @@ class UsersController < ApplicationController
       @title = "Create User"
       render 'new'
     end
-  end
-  
-  def edit
-    @title = "Edit User"
-  end
-  
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
-      flash[:success] = "Profile Updated"
-      redirect_to @user
-    else
-      @title = "Edit User"
-      render 'edit'
-    end
-  end
-  
-  private
-  
-  def authenticate
-    deny_access unless signed_in?
-  end
-  
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_path) unless current_user?(@user)
   end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,20 +1,20 @@
 require 'spec_helper'
 
 describe UsersController do
-
+  
   render_views
-
+  
   describe "GET 'show'" do
-
+    
     before (:each) do
       @user = Factory(:user)
     end
-
+    
     it "should be successful" do
       get :show, :id => @user
       response.should be_success
     end
-
+    
     it "should find the right user" do
       get :show, :id => @user
       assigns(:user).should == @user
@@ -27,199 +27,69 @@ describe UsersController do
       get 'new'
       response.should be_success
     end
-
+    
     it "should have the right title" do
       get 'new'
       response.should have_selector("title", :content => "Create User")
     end
-
+    
   end
-
+  
   describe "POST 'create'" do
-
+    
     describe "failure" do
-
+      
       before(:each) do
         @attr = { :name => "", :robot => "", :password => "", :password_confirmation => ""}
       end
-
+      
       it "should not create a user" do
         lambda do
           post :create, :user => @attr
         end.should_not change(User, :count)
       end
-
+      
       it "should have the right title" do
         post :create, :user => @attr
         response.should have_selector("title", :content => "Create User")
       end
-
+      
       it "should render the 'new' page" do
         post :create, :user => @attr
         response.should render_template('new')
       end
-
+      
     end
-
+    
     describe "success" do
-
+      
       before(:each) do
         @attr = { :name => "Kaiya" , :robot => "databot.png", :password => "foo", :password_confirmation => "foo" }
       end
-
+      
       it "should create a user" do
         lambda do
           post :create, :user => @attr
         end.should change(User, :count).by(1)
       end
-
+      
       it "should redirect to the show page" do
         post :create, :user => @attr
         response.should redirect_to(user_path(assigns(:user)))
       end
-
+      
       it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to Dinner with Family/i
       end
-
+      
       it "should sign the user in" do
         post :create, :user => @attr
         controller.should be_signed_in
       end
-
+      
     end
-
+      
   end
-
-  describe "GET 'edit'" do
-
-    before(:each) do
-      @user = Factory(:user)
-      test_sign_in(@user)
-    end
-
-    it "should be successful" do
-      get :edit, :id => @user
-      response.should be_success
-    end
-
-    it "should have the right title" do
-      get :edit, :id => @user
-      response.should have_selector("title", :content => "Edit User")
-    end
-
-  end
-
-  describe "PUT 'update'" do
-
-    before(:each) do
-      @user = Factory(:user)
-      test_sign_in(@user)
-    end
-
-    describe "failure" do
-      before(:each) do
-        @attr = { :name => "", :robot => "", :password => "", :password_confirmation => ""}
-      end
-
-      it "should render the edit page" do
-        put :update, :id => @user, :user => @attr
-        response.should render_template('edit')
-      end
-
-      it "should have the right title" do
-        put :update, :id => @user, :user => @attr
-        response.should have_selector("title", :content => "Edit User")
-      end
-    end
-
-
-    describe "success" do
-
-      before(:each) do
-        @attr = { :name => "New Name", :robot => "tankbot", :password => "bar", :password_confirmation => "bar"}
-      end
-
-      it "should change the user's attributes" do
-        put :update, :id => @user, :user => @attr
-        @user.reload
-        @user.name.should == @attr[:name]
-        @user.robot.should == @attr[:robot]
-      end
-
-      it "should redirect to the uer show page" do
-        put :update, :id => @user, :user => @attr
-        response.should redirect_to(user_path(@user))
-      end
-
-      it "should have a flash message" do
-        put :update, :id => @user, :user => @attr
-        flash[:success].should =~ /Updated/
-      end
-
-    end
-
-  end
-  
-  describe "authentication of edit/update pages" do
-    
-    before(:each) do
-      @user = Factory(:user)
-    end
-    
-    describe "for non-signed-in users" do
-      
-      it "should deny access to 'edit'" do
-        get :edit, :id => @user
-        response.should redirect_to(selectuser_path)
-      end
-      
-      it "should deny access to 'update'" do
-        put :update, :id => @user
-        response.should redirect_to(selectuser_path)
-      end
-      
-    end
-    
-    describe "for signed-in users" do
-      
-      before(:each) do
-        wrong_user = Factory(:user, :name => "Bobinator")
-        test_sign_in(wrong_user)
-      end
-      
-      it "should require matching users for 'edit'" do
-        get :edit, :id => @user
-        response.should redirect_to(root_path)
-      end
-      
-      it "should require matching users for 'update'" do
-        put :update, :id => @user, :user => {}
-        response.should redirect_to(root_path)
-      end
-      
-    end
-    
-  end
-  
-  # describe "GET 'input'" do
-  #   
-  #   before(:each) do
-  #     @user = Factory(:user)
-  #     test_sign_in(@user)
-  #   end
-  #   
-  #   it "should be successful" do
-  #     get :input, :id => @user
-  #     response.should be_success
-  #   end
-  #   
-  #   it "should have the right title" do
-  #     get :input, :id => @user
-  #     response.should have_selector("title", :content => "Input Vegetables")
-  #   end
-  #   
-  # end
 
 end
